09618a222ad5f760b8117b2c3aa3d92716bbd02f
Adrian Liechti <adrian@monobox.ch>
2026-02-15 21:55:20 +0000

    remove openai hack

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [
-      { find: 'openai', replacement: path.resolve(__dirname, 'src/lib/openai-browser.ts') },
-      { find: 'openai-original', replacement: path.dirname(require.resolve('openai')) },
-    ],
-  },
   plugins: [
     react({
       babel: {
